8d2e5247e18a3db15b356e0b0f8db9c45c426055
Waheed Naseri <waheed.reyhan@gmail.com>
2026-07-14 10:31:34 +0430

    Add responsive navigation

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Portfolio Loaded");
+const menu = document.querySelector("#mobile-menu");
+
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click",()=>{
+
+    navLinks.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
